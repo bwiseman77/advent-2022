5ddaea643f2380df1f5a6cbb4d57f4445c99c225
Brett <brett.wiseman@comcast.net>
2022-12-05 12:47:18 -0500

    day04

---
+++ Day04/program1.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+import sys
+
+def main():
+    count = 0
+    while (line:=sys.stdin.readline().strip()):
+        I1,I2 = line.split(",")
+
+        r1 = I1.split("-")
+        r2 = I2.split("-")
+        
+        if(int(r1[0]) >= int(r2[0]) and int(r1[1]) <= int(r2[1])):
+            count += 1
+        elif(int(r2[0]) >= int(r1[0]) and int(r2[1]) <= int(r1[1])):
+            count += 1
+
+
+    print(count)
+           
+
+if __name__ == "__main__":
+    main()
